14320bcb76cdc40b92acf6b7b244ce7c0ecb7df2
Malte Wirz <32659282+maltewirz@users.noreply.github.com>
2022-02-06 20:17:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Demo repo
+
+This repo contains one Cypress E2E test.
+
+* Run `npm i` to install all npm packages
+* Run `npm run cypress:open` and then click on "iframe-github.js"
+
+A new window should open and run the Cypress test.
---
+++ cypress/integration/iframe-github.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+import { skipOn } from '@cypress/skip-test'
+
+describe('Recipe: blogs__iframes', () => {
+  skipOn('firefox', () => {
+    it('do it more generically', () => {
+      const getIframeBody = (identifier) => {
+        return cy
+        .get(identifier)
+        .its('0.contentDocument.body')
+        .should('not.be.empty')
+        .then(cy.wrap)
+      }
+
+      // Visiting the page index.html and getting iframe A
+      cy.visit('index.html').contains('XHR in iframe')
+      getIframeBody('iframe[data-cy="bankid"]').as('iframeA')
+
+      cy.get('@iframeA').within(() => {
+        getIframeBody('iframe[src="https://tools.bankid.no/bankid-test/auth"]').as('iframeB')
+
+        cy.get('@iframeB').within(() => {
+          getIframeBody('iframe[src^="https://csfe.bankid.no/CentralServerFEJS"]').as('iframeC')
+
+          // Now we are in the right place and it finds the correct input element.
+          // However, normal cypress command .type() fails and we have to use library cypress-real-events,
+          // which provides an event firing system that works literally like in puppeteer
+          cy.get('@iframeC').find('input[type="tel"]').should('be.visible').realType('12345678912')
+
+          // But for the button below, this library now doesn't help anymore:
+          // "Failed to execute 'getComputedStyle' on 'Window': parameter 1 is not of type 'Element'."
+          cy.get('@iframeC').find('button[type="submit"]').should('be.visible').first().realClick()
+        })
+      })
+    })
+  })
+})
